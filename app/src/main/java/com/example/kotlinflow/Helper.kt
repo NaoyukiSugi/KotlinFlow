8794959ba96e07acec1347fda765c29a28d0e2f7
shishamo <bnsgny@gmail.com>
2021-04-15 17:55:40 +0900

    update (#3)

--- app/src/main/java/com/example/kotlinflow/Helper.kt
+++ app/src/main/java/com/example/kotlinflow/Helper.kt
@@ -1,1 +1,6 @@
+package com.example.kotlinflow
+
+import kotlinx.coroutines.flow.flowOf
+
 val userList = listOf<String>("user1", "user2", "user3", "user4", "user5")
+val flowUserList = flowOf("user6", "user7", "user8", "user9", "user10")
