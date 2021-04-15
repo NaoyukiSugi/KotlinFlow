3d0ca855eafebcc9e1232070c603c8718572b5cd
Naoyuki Sugi <bnsgny@gmail.com>
2021-04-15 19:14:31 +0900

    update

--- app/src/main/java/com/example/kotlinflow/Helper.kt
+++ app/src/main/java/com/example/kotlinflow/Helper.kt
@@ -3,4 +3,5 @@ package com.example.kotlinflow
 import kotlinx.coroutines.flow.flowOf
 
 val userList = listOf<String>("user1", "user2", "user3", "user4", "user5")
+val ageList = listOf(10, 20, 30, 40, 50)
 val flowUserList = flowOf("user6", "user7", "user8", "user9", "user10")
