872c78d5cfee27aa234f4fe16338a1776277f188
shishamo <bnsgny@gmail.com>
2021-04-15 17:36:09 +0900

    update (#1)

--- app/src/main/java/com/example/kotlinflow/MainActivity.kt
+++ app/src/main/java/com/example/kotlinflow/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.kotlinflow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import userList
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            simpleFlow().collect { user ->
+                Log.d(TAG, user)
+            }
+        }
+    }
+
+    private fun simpleFlow(): Flow<String> = flow {
+        userList.forEach { user ->
+            emit(user)
+            delay(500)
+        }
+    }
+
+    private companion object {
+        const val TAG = "MainActivity"
     }
 }
